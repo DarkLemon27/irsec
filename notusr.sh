0b7a2622e1c4b3bca180654dacb6f7be7ae93e93
Aaron Karenchak <ajk4677@rit.edu>
2019-04-19 17:08:08 -0400

    done Fix

--- notusr.sh
+++ notusr.sh
@@ -2,6 +2,7 @@
 
 ## Change passwords
 for i in princip chap queen king deejay sweats; do passwd $i
-
+done
 ## change default shells
 for i in princip chap queen king deejay sweats; do usermod -s /bin/bash $i
+done
