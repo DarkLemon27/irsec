c36c8c8f19b1836c936fccb93c959dabaa00dc29
Aaron Karenchak <ajk4677@rit.edu>
2019-04-19 17:18:16 -0400

    gotrid of stuff

--- notusr.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-## Change passwords
-for i in princip chap queen king deejay sweats; do passwd $i
-done
-## change default shells
-for i in princip chap queen king deejay sweats; do usermod -s /bin/bash $i
-done
