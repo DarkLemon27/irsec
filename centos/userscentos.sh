e075e10e67dd71849959043dc706dcae17df52fb
root <root@localhost.localdomain>
2019-04-19 22:41:11 -0400

    added passwd to usercentos.sh

--- centos/userscentos.sh
+++ centos/userscentos.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+for i in pricipal chaperone deejay kid_with_sweatpants prom_king prom_queen dbadmin; do passwd $i; done
+
 usermod -l higherup -d /home/higherup -m principal
 usermod -l babysitter -d /home/babysitter -m chaperone
 usermod -l dataminer -d /home/dataminer -m dbadmin
