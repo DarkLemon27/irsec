0a7115cff4ff8a6a92ac71f1addbb8314ba7081a
Brett Segraves <bms9294@rit.edu>
2019-04-20 02:56:14 -0400

    We gettin screwed

--- ubuntu/users.sh
+++ ubuntu/users.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+
+pass=$(read)
+
 usermod -l higherup -d /home/higherup -m principal
 usermod -l babysitter -d /home/babysitter -m chaperone
 usermod -l dataminer -d /home/dataminer -m dbadmin
@@ -8,3 +11,11 @@ usermod --expiredate 1 kid_with_sweatpants
 usermod --expiredate 1 deejay
 usermod --expiredate 1 prom_king
 usermod --expiredate 1 prom_queen
+
+echo "higherup:$pass" | chpasswd
+echo "babysitter:$pass" | chpasswd
+echo "dataminer:$pass" | chpasswd
+echo "kid_with_sweatpants:$pass" | chpasswd
+echo "deejay:$pass" | chpasswd
+echo "prom_king:$pass" | chpasswd
+echo "prom_queen:$pass" | chpasswd
