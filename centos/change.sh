c1fad71551c5f3ef68152635a2f595dd53cca2ea
root <root@localhost.localdomain>
2019-04-20 01:10:56 -0400

    more

---
+++ centos/change.sh
@@ -0,0 +1,7 @@
+for i in deejay kid_with_sweatpants prom_queen prom_king; do passwd -l $i; done
+
+
+usermod -l higherup -d /home/higherup -m principal
+usermod -l babysitter -d /home/babysitter -m chaperone
+usermod -l dataminer -d /home/dataminer -m dbadmin
+
--- centos/userscentos.sh
+++ centos/userscentos.sh
@@ -2,8 +2,5 @@
 
 for i in pricipal chaperone deejay kid_with_sweatpants prom_king prom_queen dbadmin; do passwd $i; done
 
-usermod -l higherup -d /home/higherup -m principal
-usermod -l babysitter -d /home/babysitter -m chaperone
-usermod -l dataminer -d /home/dataminer -m dbadmin
 
 
